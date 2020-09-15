1ccfcd3ddaaa3213db856da16d1f44ea05c31b96
Andrei Vasilev <andyvu@mail.ru>
2020-09-16 01:35:11 +0300

    versions bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -33,7 +31,7 @@ android {
     }
 }
 
-val kotlinVersion = "1.4.0"
+val kotlinVersion = "1.4.10"
 val ktorVersion = "1.4.0"
 
 dependencies {
@@ -41,13 +39,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.preference:preference:1.1.1")
-    implementation("com.google.android.material:material:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.0")
+    implementation("com.google.android.material:material:1.2.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.0")
     implementation("io.ktor:ktor-client-android:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
-    implementation("com.michael-bull.kotlin-result:kotlin-result-jvm:1.1.8")
+    implementation("com.michael-bull.kotlin-result:kotlin-result-jvm:1.1.9")
     implementation("org.jsoup:jsoup:1.13.1")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
@@ -55,6 +53,6 @@ dependencies {
     kapt("com.google.dagger:dagger-compiler:2.28.3")
 
     testImplementation("junit:junit:4.13")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
